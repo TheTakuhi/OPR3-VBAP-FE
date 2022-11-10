d485364d0d7eecb623060cfccd8938c4087e4bd9
Tomáš Dámek <tomasdamek123@gmail.com>
2022-11-10 11:30:51 +0100

    FE+BE connection, getCars works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Car} from "./cars/car";
+import {CarService} from "./cars/car.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'untitled';
+export class AppComponent implements OnInit{
+  public cars: Car[] | undefined;
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  public getCars(): void{
+    this.carService.getCars().subscribe((response: Car[]) => {this.cars = response},
+      (error: HttpErrorResponse) => {alert(error.message);}
+    );
+  }
 }
---
+++ src/app/cars/car.ts
@@ -0,0 +1,15 @@
+export interface Car{
+  id: number;
+  brand: string;
+  type: string;
+  bodywork: string;
+  yearOfManufacture: Date;
+  tachometer: number;
+  price: number;
+  performance: number;
+  consumption: number;
+  capacity: number;
+  fuel: string;
+  imageUrl: string;
+  description: string;
+}
