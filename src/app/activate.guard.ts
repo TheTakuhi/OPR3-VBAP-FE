142331a3b6dab71ec8fc05d3a3881d8047c1dab9
Tomáš Dámek <tomasdamek123@gmail.com>
2022-12-14 20:18:37 +0100

    Redirection if not logged in

--- src/app/activate.guard.ts
+++ src/app/activate.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate } from '@angular/router';
+import {CanActivate, Router} from '@angular/router';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -6,9 +6,13 @@ export default class ActivateGuard implements CanActivate {
 
   private can = false;
 
+  constructor(private router: Router) {
+  }
+
   canActivate() {
     console.log('ActivateGuard#canActivate called, can: ', this.can);
     if (!this.can) {
+      this.router.navigate(['/login']);
       alert('Please, log in to continue');
       return false;
     }
